e03d0a14748c997664895abf0d46dfa98d135a6f
Andiie <andre.sousa.tec@gmail.com>
2023-09-10 16:05:14 -0300

    Initial commit

---
+++ src/main/java/br/com/zuber/ZuberApplication.java
@@ -0,0 +1,13 @@
+package br.com.zuber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZuberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuberApplication.class, args);
+	}
+
+}
